403c6ea971bef2ff2c0e499846a1eb11b6e346b2
safwanshoaib <safwanshoaib2002@gmail.com>
2024-09-01 17:26:48 +0500

    FlatList for improved performance and layout styling

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
-import { View } from "react-native";
+import { FlatList } from "react-native";
 import products from "@assets/data/products";
 import ProductListItem from "@/components/ProductListItem";
 
-
 export default function TabOneScreen() {
   return (
-    <View>
-      <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/>
-    </View>
+    <FlatList
+      data={products}
+      numColumns={2}
+      contentContainerStyle={{ gap: 8, padding: 8 }}
+      columnWrapperStyle={{ gap: 8 }}
+      renderItem={({ item, index }) => <ProductListItem product={item} />}
+    />
   );
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -3,25 +3,32 @@ import Colors from "@/constants/Colors";
 import { Product } from "../app/types";
 
 interface ProductListItemProps {
-    product: Product;
+  product: Product;
 }
 
-const ProductListItem = ({product}:ProductListItemProps) =>{
-  return(
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image || "" }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || "" }}
+        style={styles.image}
+        resizeMode="contain"
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
-  )
-}
+  );
+};
 
 export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
-    padding: 10,
+    padding: 20,
+    flex: 1,
+    borderRadius: 10,
+    maxWidth: "50%",
   },
   title: {
     fontSize: 18,
