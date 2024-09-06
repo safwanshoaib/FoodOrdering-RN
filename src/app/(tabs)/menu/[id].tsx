9d90e7d0aa64154cc3e7f40e705ce5c83123972d
safwanshoaib <safwanshoaib2002@gmail.com>
2024-09-06 15:54:46 +0500

    Details screen added

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,14 +1,37 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 import Colors from "@/constants/Colors";
 import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@assets/data/products";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id.toString() === id);
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
   return (
-    <View>
-      <Stack.Screen options={{title:'Details'}}/>
-      <Text style={styles.text}>ProductDetailsScreen {id}.</Text>
+    <View style={styles.container}>
+
+      <Stack.Screen options={{ title: product?.name }} />
+
+      <Image source={{ uri: product?.image }} style={styles.image} />
+
+      <Text>Select Size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((index) => (
+          <View style={styles.size} key={index}>
+            <Text style={styles.sizeText}>{index}</Text>
+          </View>
+        ))}
+      </View>
+
+      <Text style={styles.price}>${product.price}</Text>
+
     </View>
   );
 };
@@ -16,7 +39,34 @@ const ProductDetailsScreen = () => {
 export default ProductDetailsScreen;
 
 const styles = StyleSheet.create({
-  text: {
-    color: Colors.dark.text,
+  container: {
+    backgroundColor: Colors.light.background,
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
   },
 });
